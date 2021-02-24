1c7019c4c7ad9582e229d6afeb95a768bd61b150
XinyuYang99 <xyang62@dons.usfca.edu>
2021-02-24 01:31:15 -0800

    dream home

---
+++ README.md
@@ -0,0 +1,6 @@
+## Rooms of My Dream House
+#### by Xinyu Yang
+
+
+### Overview
+This is Xinyu's house that you can visit by clicking doors or buttons or using keys and interactive among 6 rooms. Those rooms are originally made by Adobe XD.
---
+++ sketch.js
@@ -0,0 +1,324 @@
+
+/***********************************************************************************
+  Rooms of My Dream House
+  by Xinyu Yang
+------------------------------------------------------------------------------------
+  This is Xinyu's house that you can visit by clicking doors or buttons or using keys 
+  and interactive among 6 rooms. Those rooms are originally made by Adobe XD.
+
+------------------------------------------------------------------------------------
+  It uses functions like preload(), setup(), draw(), image() and anothers to illustrate
+  adobe XD's work to p5.js
+
+
+***********************************************************************************/
+
+// Array of images
+var images = [];
+var strings = [];
+var midX;
+var midY;
+var lineHeight = 24;
+var drawFunction;
+var gTextOffset = 20;
+
+function preload() {
+  images[0] = loadImage('assets/EntranceHallBack.png');
+  images[1] = loadImage('assets/DiningRoomBack.png');
+  images[2] = loadImage('assets/KitchenRoomBack.png');
+  images[3] = loadImage('assets/LivingRoomBack.png');
+  images[4] = loadImage('assets/BedRoomBack.png');
+  images[5] = loadImage('assets/BathRoomBack.png');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  midX = width/2;
+  midY = height/2;
+  startY = 60;
+
+  imageMode(CENTER);
+  textAlign(CENTER);
+  textSize(24);
+  entrance = loadImage('assets/Entrance.png');
+  chandeliersDining = loadImage('assets/ChandeliersDining.png');
+  table = loadImage('assets/Table.png');
+  fireplace = loadImage('assets/Fireplace.png');
+  kitchen = loadImage('assets/Kitchen.png');
+  chandeliersLiving = loadImage('assets/ChandeliersLiving.png');
+  sofa = loadImage('assets/Sofa.png');
+  bookShelf = loadImage('assets/bookShelf.png');
+  draw1 = loadImage('assets/Draw1.png');
+  draw2 = loadImage('assets/Draw2.png');
+  bedroom = loadImage('assets/BedRoom.png');
+  bathroom = loadImage('assets/BathRoom.png')
+
+  drawFunction = drawEntranceHall;
+
+}
+
+function draw() {
+  background(255);
+  drawFunction();
+}
+
+drawEntranceHall = function() {
+  image(images[0], midX, midY, windowWidth, windowHeight);
+  image(entrance, midX, midY, windowWidth, windowHeight);
+
+  fill(0,0,0);
+  loadArray();
+}
+
+drawDiningRoom = function() {
+  image(images[1], midX, midY, windowWidth, windowHeight);
+  image(chandeliersDining, midX, midY, windowWidth, windowHeight);
+  image(table, midX, midY, windowWidth, windowHeight);
+  image(fireplace, midX, midY, windowWidth, windowHeight);
+
+  fill(0,0,0);
+}
+
+drawKitchenRoom = function() {
+  image(kitchen, midX, midY, windowWidth, windowHeight);
+  image(images[2], midX, midY, windowWidth, windowHeight);
+
+  fill(0,0,0);
+}
+
+drawLivingRoom = function() {
+  image(images[3], midX, midY, windowWidth, windowHeight);
+  image(chandeliersLiving, midX, midY, windowWidth, windowHeight);
+  image(sofa, midX, midY, windowWidth, windowHeight);
+  image(bookShelf, midX, midY, windowWidth, windowHeight);
+  image(draw1, midX, midY, windowWidth, windowHeight);
+  image(draw2, midX, midY, windowWidth, windowHeight);
+  fill(0,0,0);
+}
+
+//-- drawOne() will draw the image at index 4 from the array
+drawBedRoom = function() {
+  image(images[4], midX, midY, windowWidth, windowHeight);
+  image(bedroom, midX, midY, windowWidth, windowHeight);
+  fill(0,0,0);
+  text("Bathroom is private location, please first knock to check if anyone's there!", midX, height - gTextOffset);
+}
+
+drawBathRoom = function() {
+  image(bathroom, midX, midY, windowWidth, windowHeight);
+  image(images[5], midX, midY, windowWidth, windowHeight);
+  fill(0,0,0);
+}
+
+
+function loadArray() {
+  strings[0] = "click the doors to next room";
+  strings[1] = "or";
+  strings[2] = "click the buttons to next room";
+  strings[3] = "or";
+  strings[4] = "press the first character for each room";
+  strings[5] = ""
+  strings[6] = "Hopes you love my home."
+  strings[7] = "";
+  fill(0);
+  for( let i = 0 ; i < strings.length; i++ ) {
+    text( strings[i], midX, startY + (i * lineHeight) )
+  }
+}
+
+// Change the drawFunction variable
+function EnterEntranceHall() {
+  drawFunction = drawEntranceHall;
+}
+
+function EnterDiningRoom() {
+  drawFunction = drawDiningRoom;
+}
+
+function EnterKitchen() {
+  drawFunction = drawKitchenRoom;
+}
+
+function EnterLivingRoom() {
+  drawFunction = drawLivingRoom;
+}
+
+function EnterBedroom() {
+  drawFunction = drawBedRoom;
+}
+
+function EnterBathroom() {
+  drawFunction = drawBathRoom;
+}
+
+function keyTyped() {
+  if (drawFunction === drawEntranceHall) {
+    if (key === 'l') {
+      EnterLivingRoom();
+    }
+    else if (key === 'd') {
+      EnterDiningRoom();
+    }
+  }
+
+  if (drawFunction === drawDiningRoom) {
+    if (key === 'k') {
+      EnterKitchen();
+    }
+    else if (key === 'l') {
+      EnterLivingRoom();
+    }
+    else if (key === 'e') {
+      EnterEntranceHall();
+    }
+  }
+
+  if (drawFunction === drawKitchenRoom) {
+    if (key === 'd') {
+      EnterDiningRoom();
+    }
+  }
+
+  if (drawFunction === drawLivingRoom) {
+    if (key === 'e') {
+      EnterEntranceHall();
+    }
+    else if (key === 'd') {
+      EnterDiningRoom();
+    }
+    else if (key === 'b') {
+      EnterBedroom();
+    }
+  }
+
+  if (drawFunction === drawBedRoom) {
+    if (key === 'l') {
+      EnterLivingRoom();
+    }
+  }
+
+  if (drawFunction === drawBathRoom) {
+    if (key === 'b') {
+      EnterBedroom();
+    }
+  }
+}
+
+function leftSign() {
+  if (mouseX <= midX / 2) {
+    if (mouseY >= midY - 100 && mouseY <= midY + 100) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function rightSign() {
+  if (mouseX >= (midX) + (midX/2)) {
+    if (mouseY >= midY - 100 && mouseY <= midY + 100) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function kitchenDoor() {
+  if (mouseX <= width /3 || mouseX >= width * 2/3) {
+    if (mouseY >= height / 6 && mouseY <= height * 2/3) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function lowerLeftssign() {
+  if (mouseX <= width /7 && mouseY >= height * 2/3) {
+    return true;
+  }
+  return false;
+}
+
+function lowerMiddlesign() {
+  if (mouseX <= midX + 150 && mouseX >= midX - 150) {
+    if (mouseY >= height * 2/3) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function lowerRightsign() {
+  if (mouseX >= width * 6/7 && mouseY >= height * 2/3) {
+    return true;
+  }
+  return false;
+}
+
+function upperLeftsign() {
+  if (mouseX <= width / 8 && mouseY <= height / 6) {
+    return true;
+  }
+  return false;
+}
+
+function upperRightsign() {
+  if (mouseX >= width * 7/ 8 && mouseY <= height / 6) {
+    return true;
+  }
+  return false;
+}
+
+function mousePressed() {
+  if (drawFunction === drawEntranceHall) {
+    if (leftSign()) {
+      EnterLivingRoom();
+    }
+    else if (rightSign()) {
+      EnterDiningRoom();
+    }
+  }
+  else if (drawFunction === drawDiningRoom) {
+    if (kitchenDoor()) {
+      EnterKitchen();
+    }
+    else if (lowerLeftssign()) {
+      EnterEntranceHall();
+    }
+    else if (lowerRightsign()) {
+      EnterLivingRoom();
+    }
+  }
+  else if (drawFunction === drawKitchenRoom) {
+    if (lowerLeftssign()) {
+      EnterDiningRoom();
+    }
+    else if (lowerRightsign()) {
+      EnterDiningRoom();
+    }
+  }
+  else if (drawFunction === drawLivingRoom) {
+    if (upperLeftsign()) {
+      EnterEntranceHall();
+    }
+    else if (upperRightsign()) {
+      EnterDiningRoom();
+    }
+    else if (lowerMiddlesign()) {
+      EnterBedroom();
+    }
+  }
+  else if (drawFunction === drawBedRoom) {
+    if (lowerLeftssign()) {
+      EnterLivingRoom();
+    }
+    else if (lowerRightsign()) {
+      EnterBathroom();
+    }
+  }
+  else if (drawFunction === drawBathRoom) {
+    if (lowerLeftssign()) {
+      EnterBedroom();
+    }
+  }
+}
